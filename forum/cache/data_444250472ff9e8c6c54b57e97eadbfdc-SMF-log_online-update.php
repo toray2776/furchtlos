095cc84a711f8783ed715af79100e10cadee16d2
Philipp Bechtel <philipp.bechtel@kernpunkt.de>
2019-11-13 13:24:41 +0100

    Initialization

---
+++ forum/cache/data_444250472ff9e8c6c54b57e97eadbfdc-SMF-log_online-update.php
@@ -0,0 +1,1 @@
+<?php if (!defined('SMF')) die; if (1573647803 < time()) $expired = true; else{$expired = false; $value = 'i:1573647773;';}?>
---
+++ forum/cache/data_444250472ff9e8c6c54b57e97eadbfdc-SMF-user_last_visit-1.php
@@ -0,0 +1,1 @@
+<?php if (!defined('SMF')) die; if (1573665575 < time()) $expired = true; else{$expired = false; $value = 's:1:"0";';}?>
